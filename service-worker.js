36f2aa87a4def70e9fe4f86ed2d49fd92f9c98f5
nadiiahonda34-svg <nadiiahonda34@gmail.com>
2026-09-03 20:42:38 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="kindred-community-v5";
+const CACHE="kindred-community-v6";
 const SHELL=["/app.html","/app.css","/kindred-app.js","/firebase-app.js","/manifest.webmanifest","/kindred-icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
